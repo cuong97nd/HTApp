741c5b56de4072ad9ba10ee445271a8efd479f4e
cuong <buihoangcuong972@gmail.com>
2021-07-24 17:25:14 +0900

    fix chart

--- src/components/_dashboard/app/AppConversionRates.js
+++ src/components/_dashboard/app/AppConversionRates.js
@@ -3,49 +3,125 @@ import ReactApexChart from 'react-apexcharts';
 // material
 import { Box, Card, CardHeader } from '@material-ui/core';
 // utils
-import { fNumber } from '../../../utils/formatNumber';
-//
-import { BaseOptionChart } from '../../charts';
-
 // ----------------------------------------------------------------------
 
-const CHART_DATA = [{ data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380] }];
+const series = [
+  {
+    name: 'Actual',
+    data: [
+      {
+        x: '2011',
+        y: 12,
+        goals: [
+          {
+            name: 'Expected',
+            value: 14,
+            strokeWidth: 5,
+            strokeColor: '#775DD0'
+          }
+        ]
+      },
+      {
+        x: '2012',
+        y: 44,
+        goals: [
+          {
+            name: 'Expected',
+            value: 54,
+            strokeWidth: 5,
+            strokeColor: '#775DD0'
+          }
+        ]
+      },
+      {
+        x: '2013',
+        y: 54,
+        goals: [
+          {
+            name: 'Expected',
+            value: 52,
+            strokeWidth: 5,
+            strokeColor: '#775DD0'
+          }
+        ]
+      },
+      {
+        x: '2014',
+        y: 66,
+        goals: [
+          {
+            name: 'Expected',
+            value: 65,
+            strokeWidth: 5,
+            strokeColor: '#775DD0'
+          }
+        ]
+      },
+      {
+        x: '2015',
+        y: 81,
+        goals: [
+          {
+            name: 'Expected',
+            value: 66,
+            strokeWidth: 5,
+            strokeColor: '#775DD0'
+          }
+        ]
+      },
+      {
+        x: '2016',
+        y: 67,
+        goals: [
+          {
+            name: 'Expected',
+            value: 70,
+            strokeWidth: 5,
+            strokeColor: '#775DD0'
+          }
+        ]
+      }
+    ]
+  }
+];
+
+const options = {
+  chart: {
+    height: 350,
+    type: 'bar'
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true
+    }
+  },
+  colors: ['#00E396'],
+  dataLabels: {
+    formatter: function ok(val, opt) {
+      const { goals } = opt.w.config.series[opt.seriesIndex].data[opt.dataPointIndex];
+
+      if (goals && goals.length) {
+        return `${val} / ${goals[0].value}`;
+      }
+      return val;
+    }
+  },
+  legend: {
+    show: true,
+    showForSingleSeries: true,
+    customLegendItems: ['Actual', 'Expected'],
+    markers: {
+      fillColors: ['#00E396', '#775DD0']
+    }
+  }
+};
 
 export default function AppConversionRates() {
-  const chartOptions = merge(BaseOptionChart(), {
-    tooltip: {
-      marker: { show: false },
-      y: {
-        formatter: (seriesName) => fNumber(seriesName),
-        title: {
-          formatter: (seriesName) => `#${seriesName}`
-        }
-      }
-    },
-    plotOptions: {
-      bar: { horizontal: true, barHeight: '28%', borderRadius: 2 }
-    },
-    xaxis: {
-      categories: [
-        'ビタミンA',
-        'ビタミンB',
-        'ビタミンC',
-        'ビタミンD',
-        'ビタミンA',
-        'ビタミンB',
-        'ビタミンC',
-        'ビタミンD',
-        'ビタミンA',
-        'ビタミンB'
-      ]
-    }
-  });
-
   return (
     <Card>
       <CardHeader title="体の状況" subheader="(+43%) than last year" />
       <Box sx={{ mx: 3 }} dir="ltr">
-        <ReactApexChart type="bar" series={CHART_DATA} options={chartOptions} height={364} />
+        <ReactApexChart options={options} series={series} type="bar" height={350} />
       </Box>
     </Card>
   );
