41620f5327c4db1c7ebd16c28c1c223ff826537d
Như Thuỷ <nhuthuy@ava.local>
2021-08-07 18:14:31 +0900

    add_auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
+import React from 'react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { AmplifyAuthenticator, AmplifySignOut, AmplifySignUp } from '@aws-amplify/ui-react';
+import Amplify from 'aws-amplify';
 // routes
 import Router from './routes';
 // theme
 import ThemeConfig from './theme';
 // components
 import ScrollToTop from './components/ScrollToTop';
+import awsconfig from './aws-exports';
 
+Amplify.configure(awsconfig);
 // ----------------------------------------------------------------------
+const AuthStateApp = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
 
-export default function App() {
-  return (
+  React.useEffect(
+    () =>
+      onAuthUIStateChange((nextAuthState, authData) => {
+        setAuthState(nextAuthState);
+        setUser(authData);
+      }),
+    []
+  );
+
+  return authState === AuthState.SignedIn && user ? (
     <ThemeConfig>
       <ScrollToTop />
       <Router />
     </ThemeConfig>
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[{ type: 'username' }, { type: 'password' }, { type: 'email' }]}
+      />
+    </AmplifyAuthenticator>
   );
-}
+};
+
+export default AuthStateApp;
